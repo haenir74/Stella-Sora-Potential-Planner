43ccd93f6d6abf0ebb018d1373586b8d5d59d1ab
haenir74 <haenir74@gmail.com>
2026-02-04 10:34:36 +0900

    Feat: 뉴크루 추가

--- app/build.bat
+++ app/build.bat
@@ -12,7 +12,7 @@ del /s /q *.pyc
 
 echo.
 echo [STEP 2] Building...
-python -m PyInstaller --clean --noconsole --onedir --name="PotentialPlanner" --distpath "%BUILD_DIR%\dist" --workpath "%BUILD_DIR%\build" --specpath "%BUILD_DIR%" --paths "src" "run.py"
+venv\Scripts\python -m PyInstaller --clean --noconsole --onedir --name="PotentialPlanner" --distpath "%BUILD_DIR%\dist" --workpath "%BUILD_DIR%\build" --specpath "%BUILD_DIR%" --paths "src" "run.py"
 
 echo.
 echo [STEP 3] Copying Resources...
